47e4263dd1bb3ece4c5e620a2f3085c0821fe3dd
Mithu A Quayium <cemithu06@gmail.com>
2021-03-10 21:11:51 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,38 +16,45 @@ class ListViewWidget extends StatefulWidget{
 
 class _ListViewWidgetState extends State<ListViewWidget> {
   int listNum = 1;
-  TextEditingController _number;
+  TextEditingController _todo;
+  List todo_list = <Widget>[];
 
   @override
   initState(){
-    _number = new TextEditingController();
+    _todo = new TextEditingController();
     super.initState();
   }
 
-  List GenerateList(int num) {
-    final List list = <Widget>[];
-    for( var i = 1; i <= num; i++ ) {
-      list.add(Card(
+  addToDo( title, subtitle ) {
+    setState(() {
+      this.todo_list.add(Card(
         child: ListTile(
           leading: Icon(
             Icons.account_circle,
             size: 30,
           ),
           trailing: Icon(
-            Icons.favorite
+              Icons.favorite
           ),
-          title: Text('Index $i'),
-          subtitle: Text( '$i' ),
+          title: Text(title),
+          subtitle: Text(subtitle),
           isThreeLine: true,
-
         ),
       ));
+    });
+
+  }
+
+  GenerateList(int num) {
+    final List list = <Widget>[];
+    for( var i = 1; i <= num; i++ ) {
+      addToDo('Index $i','$i');
     }
-    return list;
   }
 
   @override
   Widget build(BuildContext context) {
+    GenerateList(this.listNum);
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
@@ -55,6 +62,22 @@ class _ListViewWidgetState extends State<ListViewWidget> {
       ),
       body: ListView(
         children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.redAccent,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  height: 100,
+                ),
+              )
+            ],
+          ),
           Row(
             children: [
               IconButton(icon: Icon(Icons.add), onPressed: (){
@@ -74,9 +97,15 @@ class _ListViewWidgetState extends State<ListViewWidget> {
             ],
           ),
           Column(
-            children: GenerateList(this.listNum),
+            children: this.todo_list,
           ),
         ]
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.amber,
+        child: Container(
+          height: 50.0,
+        ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){
         showDialog(
@@ -87,20 +116,15 @@ class _ListViewWidgetState extends State<ListViewWidget> {
               content: Column(
                 children: [
                   TextField(
-                    decoration: InputDecoration(hintText: "Update Info"),
-                    keyboardType: TextInputType.number,
-                    controller: _number,
+                    decoration: InputDecoration(hintText: "Insert Todo"),
+                    keyboardType: TextInputType.text,
+                    controller: _todo,
                   )
                 ],
               ),
               actions: [
                 FlatButton(onPressed: (){
-                  setState(() {
-                    var n = int.parse(_number.text);
-                    if( n >= 0 ) {
-                      this.listNum = n;
-                    }
-                  });
+                  addToDo(_todo.text, _todo.text);
                 }, child: Text('Yes')),
                 FlatButton(onPressed: (){}, child: Text('No'))
               ],
@@ -111,6 +135,7 @@ class _ListViewWidgetState extends State<ListViewWidget> {
         backgroundColor: Colors.amber,
         child: Text("Todo"),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
